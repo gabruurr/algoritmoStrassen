02ecb21aa46e01bc32177831313d10d3d2cf3f91
unknown <gabryelaraujo2019@gmail.com>
2025-04-24 22:09:57 -0300

    feat: adicionado algoritmo de strassen

--- main.py
+++ main.py
@@ -11,3 +11,35 @@ def mutiplicacaoPadrao(A, B):
             for k in range(n):
                 C[i][j] += A[i][k] * B[k][j]
     return C
+
+def strassen(A, B):
+    n = A.shape[0]
+    if n == 1:
+        return A * B
+    else:
+        mid = n // 2
+        A11 = A[:mid, :mid]
+        A12 = A[:mid, mid:]
+        A21 = A[mid:, :mid]
+        A22 = A[mid:, mid:]
+        B11 = B[:mid, :mid]
+        B12 = B[:mid, mid:]
+        B21 = B[mid:, :mid]
+        B22 = B[mid:, mid:]
+
+        M1 = strassen(A11 + A22, B11 + B22)
+        M2 = strassen(A21 + A22, B11)
+        M3 = strassen(A11, B12 - B22)
+        M4 = strassen(A22, B21 - B11)
+        M5 = strassen(A11 + A12, B22)
+        M6 = strassen(A21 - A11, B11 + B12)
+        M7 = strassen(A12 - A22, B21 + B22)
+
+        C11 = M1 + M4 - M5 + M7
+        C12 = M3 + M5
+        C21 = M2 + M4
+        C22 = M1 - M2 + M3 + M6
+
+        cima = np.hstack((C11, C12))
+        baixo = np.hstack((C21, C22))
+        return np.vstack((cima, baixo))
